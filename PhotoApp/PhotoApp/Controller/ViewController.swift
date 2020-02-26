b9948616ba727280620e2dd0ce777ba860f3af1d
Haroldeuh <harold.aholoukpe@gmail.com>
2020-02-26 18:49:31 +0100

    debut projet, ajout MVC

---
+++ PhotoApp/PhotoApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by harold aholoukpe on 26/02/2020.
+//  Copyright © 2020 harold aholoukpe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
